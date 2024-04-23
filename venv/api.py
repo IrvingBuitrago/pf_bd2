94d993ec9cc8d93dd972e4d6ef425c25a69959a3
IrvingBuitrago <irvingbuitrago29@gmail.com>
2024-04-22 23:40:10 -0500

    login y registro de estudiantes listos

--- venv/api.py
+++ venv/api.py
@@ -6,47 +6,59 @@ app = Flask(__name__)
 
 instancia = myconex
 
-@app.route('/', methods= ['GET', 'POST'])
-def dashboard():
+@app.route('/dash/estudiantes', methods= ['GET', 'POST'])
+def dashboard_estudiante():
     return render_template('dash.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/dash/admin', methods=['GET'])
+def dashboard_admin():
+    return render_template('dash_admin.html')
+
+@app.route('/', methods=['GET', 'POST'])
 def logIn():
     if request.method == 'POST':
-        id = request.form['id']
+        username = request.form['username']
         password = request.form['password']
         instancia.conectar()
-        query = 'SELECT passwrd FROM students WHERE id = %s'
-        result = instancia.consultar(query, (id))
+        query = 'SELECT passwrd FROM users WHERE user_name = %s'
+        result = instancia.consultar(query, (username))
         try:
             if result and pbkdf2_sha256.verify(password, result[0]):
-                # Si hay un resultado, redirigir al usuario al dashboard
-                instancia.cerrar_conex()
-                return redirect(url_for('dashboard'))
-            else:
-                # Si no hay resultado, mostrar un mensaje de error
-                instancia.cerrar_conex()
-                return render_template('login.html', error='Datos no encontrados')
+                query = 'SELECT rol FROM users WHERE user_name = %s'
+                result = instancia.consultar(query, (username,))
+                if result:
+                    if result[0] == 'Estudiante':
+                        # Si el usuario es estudiante, redirigir al dashboard de estudiantes
+                        instancia.cerrar_conex()
+                        return redirect(url_for('dashboard_estudiante'))
+                    else:
+                        # Si el usuario no es estudiante, redirigir al dashboard de administrador
+                        instancia.cerrar_conex()
+                        return redirect(url_for('dashboard_admin'))
         except Exception as e:
             return render_template('login.html', error=f'{str(e)}')
+        else:
+            # Si no hay resultado, mostrar un mensaje de error
+            instancia.cerrar_conex()
+            return render_template('login.html', error='Datos no encontrados')
     else:
         return render_template('login.html')
 
-@app.route('/registro/estudiante', methods= ['GET', 'POST'])
-def registro_studiante():
+@app.route('/registro', methods= ['GET', 'POST'])
+def registro():
     if request.method == 'POST':
-        id = request.form['id']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
         first_name = request.form['first_name']
         last_name = request.form['last_name']
-        email = request.form['email']
-        password = request.form['password']
-        cellphone = request.form['cellphone']
+        rol = request.form['rol']
         career = request.form['career']
-        university = request.form['university']
+        cellphone = request.form['cellphone']
         year_study = request.form['year_study']
         instancia.conectar()
-        query = 'SELECT * FROM students WHERE id = %s'
-        result = instancia.consultar(query, (id))
+        query = 'SELECT * FROM users WHERE user_name = %s'
+        result = instancia.consultar(query, (username))
         if result:
             # Si hay un resultado, mostrar un mensaje de error
             instancia.cerrar_conex()
@@ -55,11 +67,11 @@ def registro_studiante():
             try:
                 # Si no hay resultado, agregar al usuario
                 password_hash = pbkdf2_sha256.hash(password)
-                query2 = 'INSERT INTO students (id, first_name, last_name, email, passwrd, cellphone, career, university, year_study) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
-                values = (id, first_name, last_name, email, password_hash, cellphone, career, university, year_study)
+                query2 = 'INSERT INTO users (user_name, passwrd, email, first_name, last_name, rol, career, cellphone, year_study) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                values = (username, password_hash, email, first_name, last_name, rol, career, cellphone, year_study)
                 instancia.insertar(query2, values)
                 instancia.cerrar_conex()
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('dashboard_estudiante'))
             except Exception as e:
                 instancia.cerrar_conex()
                 # Si ocurre un error, mostrar mensaje de error genérico
@@ -67,6 +79,37 @@ def registro_studiante():
     else:
         return render_template('registro_estudiante.html')
 
+@app.route('/registro/empresas', methods= ['GET', 'POST'])
+def registro_empresas():
+    if request.method == 'POST':
+        name = request.form['name']
+        direction = request.form['direction']
+        telephone = request.form['telephone']
+        email = request.form['email']
+        contact_name = request.form['contact_name']
+        contact_last_name = request.form['contact_last_name']
+        contact_email = request.form['contact_email']
+        contact_telephone = request.form['contact_telephone']
+        instancia.conectar()
+        query = 'SELECT * FROM enterprises WHERE name_ent = %s'
+        result = instancia.consultar(query, (name))
+        if result:
+            # Si hay un resultado, mostrar un mensaje de error
+            instancia.cerrar_conex()
+            return render_template('registro_empresa.html', error= 'Esta empresa ya existe')
+        else:
+            try:
+                query2 = 'INSERT INTO enterprises (name_ent, direction, telephone, email, contact_name, contact_last_name, contact_email, contact_telephone) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+                values = (name, direction, telephone, email, contact_name, contact_last_name, contact_email, contact_telephone)
+                instancia.insertar(query2, values)
+                instancia.cerrar_conex()
+                return redirect(url_for('dashboard_admin'))
+            except Exception as e:
+                instancia.cerrar_conex()
+                # Si ocurre un error, mostrar mensaje de error genérico
+                return render_template('registro_empresa.html', error=f'Error en el registro. Error: {str(e)}')
+    else:
+        return render_template('registro_empresa.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ venv/conex.py
@@ -0,0 +1,3 @@
+from dml import DML
+
+myconex = (DML("localhost", "root", "Iiebc04299?", "practicas_profesionales", 3305))
